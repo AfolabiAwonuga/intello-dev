87c50f19d1f75dce7b6f031effc0591f60c33a5e
AfolabiAwonuga <afolabiawonuga@gmail.com>
2024-09-14 19:24:40 +0100

    testing coolify webhook

--- app/dashboard/items/page.tsx
+++ app/dashboard/items/page.tsx
@@ -4,7 +4,7 @@ import ToggleNav from "@/components/ToggleNav";
 export default function ItemsPage() {
   return (
     <div className="flex justify-center items-center row-span-2">
-      <p className="text-6xl ">Coming Soon</p>
+      <p className="text-6xl ">Coming Soon </p>
     </div>
   );
 }
